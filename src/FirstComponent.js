db4cd570d5f30d1f5c1c9c2c32ff7b966e31c00c
Ruma <matteoh8@hotmail.it>
2023-07-03 16:10:06 +0200

    icona ok

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -1,9 +1,13 @@
 import React from "react";
 import './FirstComponent.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 
 const FirstComponent = () => {
   return (
     <div>
+        <FontAwesomeIcon icon={faUser} />
         <h1>First Component</h1>
     </div>
   )
