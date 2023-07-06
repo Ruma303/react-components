e53b5428561491dab677ec642df295576226f5a8
Ruma303 <matteoh8@gmail.com>
2023-07-06 11:36:05 +0200

    aggiunti stili inline

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -5,10 +5,17 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 
 const FirstComponent = () => {
+    const myStyle = {
+        color: "orange"
+    }
+    const p1 = 'Inline style da oggetto esterno';
+    const p2 = 'Inline style da oggetto interno';
   return (
     <div>
         <FontAwesomeIcon icon={faUser} />
-        <h1>First Component</h1>
+        <h1 className="title">First Component</h1>
+        <p style={myStyle}>{p1}</p>
+        <p style={{color: "hotpink", textAlign: "end"}}>{p2}</p>
     </div>
   )
 }
